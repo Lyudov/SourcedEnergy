fe6d619cd11e0792c879d1a8bf6d6e733737fc8c
Lyudov <dlyudov@gmail.com>
2023-07-29 17:09:58 +0300

    Create 404 page not found

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
-  exports: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, PageNotFoundComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [HeaderComponent, FooterComponent, PageNotFoundComponent],
 })
 export class CoreModule {}
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
-  isLogged = true;
+export class HeaderComponent {
+  get isLoggedIn() {
+    return this.authService.isLoggedIn;
+  }
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  get user() {
+    return this.authService.user;
+  }
+  constructor(private authService: AuthService, private router: Router) {}
 }
